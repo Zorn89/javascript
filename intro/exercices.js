d1d2eca84b9b534636fafa44991e39a6690c0819
Zorn89 <jan.zorn@tn.techstarter.de>
2025-03-03 13:47:12 +0100

    merg

---
+++ intro/exercices.js
@@ -0,0 +1,5 @@
+let my_name ="Master";
+let alter = 35;
+
+console.log(my_name);
+console.log(alter);
---
+++ intro/exercises2.js
@@ -0,0 +1,33 @@
+// zwei Variablen
+let zahl1 = 10;
+let zahl2 = 5;
+
+// Addition
+let addition = zahl1 + zahl2;
+console.log("Addition: " + addition); // Ausgabe: Addition: 15
+
+// Subtraktion
+let subtraktion = zahl1 - zahl2;
+console.log("Subtraktion: " + subtraktion); // Ausgabe: Subtraktion: 5
+
+// Multiplikation
+let multiplikation = zahl1 * zahl2;
+console.log("Multiplikation: " + multiplikation); // Ausgabe: Multiplikation: 50
+
+// Division
+let division = zahl1 / zahl2;
+console.log("Division: " + division); // Ausgabe: Division: 2
+
+// Längere Formel mit Klammern
+let formel = (zahl1 + zahl2) * (zahl1 - zahl2);
+console.log("Formel: " + formel); // Ausgabe: Formel: 75
+
+// Hochzahl (Potenz)
+let potenz = Math.pow(zahl1, 2); // zahl1 hoch 2 altern. zahl1 ** 2
+console.log("Potenz: " + potenz); // Ausgabe: Potenz: 100
+
+// Wurzelrechnung
+let wurzel = Math.sqrt(zahl1); // Quadratwurzel von zahl1
+console.log("Wurzel: " + wurzel); // Ausgabe: Wurzel: 3.1622776601683795
+
+
